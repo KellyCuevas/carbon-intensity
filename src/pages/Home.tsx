e87a79a85c65ab6bf52429e3aa8e397f426ee1a1
kcuevas <cuev73@live.com>
2024-05-06 10:25:34 -0400

    feature(Home): add index variable and logic to assign value based on total overall intensity calc and display in correct color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { getCurrentOverallIntensity } from "../api";
 import { useQuery } from "@tanstack/react-query";
 import calculateTotalAverage from "../utils/calculateTotalAverage";
@@ -15,12 +15,31 @@ const Home = () => {
       (segment: CurrOverallIntensityData) => segment.intensity.actual !== null
     )
   );
+
+  let index = "";
+
+  if (total) {
+    if (total < 35) index = "very-low";
+    if (total >= 35 && total < 110) index = "low";
+    if (total >= 110 && total < 190) index = "moderate";
+    if (total >= 190 && total < 270) index = "high";
+    if (total >= 270) index = "very-high";
+  }
+
   return (
     <>
       <h1>Carbon Intensity</h1>
       <div>
         <h3>The Current Average Overall Carbon Intensity Today Is:</h3>
-        {total ? total : "calculating..."}
+        {total ? (
+          <span className={`overall-stat ${index}`}>
+            {total}
+            <br />
+            {index.toUpperCase()}
+          </span>
+        ) : (
+          "calculating..."
+        )}
       </div>
     </>
   );
